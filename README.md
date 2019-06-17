5ec3df4c1a743fd68e21c8eb0ecb42c0f00801cf
Dhaval Shah <dhaval201279@rediffmail.com>
2019-06-17 13:01:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# About
+
+### Reference Implementation
+Reference implementation done as part of spring tips for [Reactive MySQL with Jasync SQL and R2DBC](https://youtu.be/DvO4zLVDkMs)
+
---
+++ src/test/java/com/its/reactivemysqljaasyncr2dbc/ReactiveMysqlJaasyncR2dbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.its.reactivemysqljaasyncr2dbc;
+
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveMysqlJaasyncR2dbcApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
